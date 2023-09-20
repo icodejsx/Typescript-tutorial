9bc6e8814d5d77503eaf3ecd762209506f4cd85c
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2023-09-20 14:41:24 +0100

    working

--- public/app.js
+++ public/app.js
@@ -3,4 +3,13 @@
 // console.log(anchor.href)
 // const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
+// console.log(form.children);
+// inputs 
+const type = document.querySelector('#type');
+const toform = document.querySelector('#toform');
+const detials = document.querySelector('#detials');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toform.value, detials.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,28 @@
 // const form = document.querySelector('form')!;
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
+// console.log(form.children);
+
+// inputs 
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toform = document.querySelector('#toform') as HTMLInputElement;
+const details = document.querySelector('#detials') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        toform.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
+
+let car: any;
+car = "text" 
+car = 45
+
+console.log(car)
